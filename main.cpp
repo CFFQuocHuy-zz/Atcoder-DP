a70800a38a1d17ced65bfbf0f042a6eaedcda660
Hexcalibearuy <46992323+CFFQuocHuy@users.noreply.github.com>
2020-02-07 17:21:47 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*
+    LEARNING DP:
+        +
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DB(_x)      cout << #_x << " is " << (_x) << "\n";
+#define FOR(_a,_b)  for (_a = 0; _a < _b; ++_a)
+
+using LL = long long;
+using LD = long double;
+using VI = vector<int>;
+using PI = pair<int,int>;
+
+void Excalibur(){
+    int n; cin >> n; VI a(n);
+    for (auto &x: a) cin >> x;
+
+    VI d(n, 1e9);
+    int i;
+    FOR (i,n){
+        if (i-1 >= 0)
+    }
+}
+
+int main(){
+ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+freopen("INPUT.in", "r", stdin);
+freopen("OUTPUT.out", "w", stdout);
+
+Excalibur();
+return 0;}
